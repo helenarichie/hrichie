49d80fc02b1130310d18fcb326a4577946eb0d68
Helena Richie <helenarichie@gmail.com>
2020-07-13 14:19:49 -0400

    Try something

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "value": "#ffffff"
       },
       "shape": {
-        "type": "star",
+        "type": "circle",
         "stroke": {
           "width": 0,
           "color": "#000000"
